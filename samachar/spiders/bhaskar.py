b8bd3c9a13df80c7d44e5b83d7d03d75c16157b0
raxy <imraxy@gmail.com>
2017-04-03 15:17:13 +0530

    fixed broken bhaskar spider

--- samachar/spiders/bhaskar.py
+++ samachar/spiders/bhaskar.py
@@ -18,28 +18,29 @@ class BhaskarSpider(scrapy.Spider):
 
     def parse(self, response):
      
-        news = Selector(response).xpath('//ul[@id="flicker_eknazar"]/li')
-        print news
-        for news in news:
+        newss = Selector(response).xpath('//ul[@class="trending-flick"]/li')
+        #print news
+        for news in newss:
             item = SamacharItem()
-            item['title'] = news.xpath('p/a/text()').extract()[0]
-            item['url'] = news.xpath('p/a/@href').extract()[0]
-            item['img_title'] = news.xpath('div/a/@title').extract()[0]
-            if news.xpath('div/a/img/@src').extract():
-            	item['img_urls'] = news.xpath('div/a/img/@src').extract()[0]
+            item['title'] = news.xpath('div/a/text()').extract_first()
+            item['url'] = news.xpath('div/a/@href').extract_first()
+            item['img_title'] = news.xpath('div/a/@title').extract_first()
+            if news.xpath('a/img/@src').extract_first():
+                item['img_urls'] = news.xpath('a/img/@src').extract_first()
             else:
-                item['img_urls'] = news.xpath('div/a/img/@data-original').extract()[0]
-            #item['shortdesc'] = news.xpath('p/text()').extract()[0]
+                item['img_urls'] = news.xpath('a/img/@data-original').extract_first()
 
             if item['url']:
                 request = scrapy.Request(url=item['url'], callback=self.parse_detail_page, meta={'item':item}, dont_filter=True)   
             request.meta['item'] =item
+            
             yield request
 
     def parse_detail_page(self, response):
         item = response.meta['item']
         detailPageSelector = Selector(response)
-        item['description'] = detailPageSelector.xpath('//div[@id="fontSize-2" or @class="mainText"]/div').extract_first()
+        item['shortdesc'] = item['description'] = detailPageSelector.xpath('//div[@id="fontSize-2" or @class="mainText"]/div').extract_first()
+
         
         yield item
 
