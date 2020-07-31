62775be268caba03795975b5e9c07d87e1f16fa0
imraxy <imraxy@gmail.com>
2020-07-31 15:11:34 +0530

    Update settings.py

--- samachar/settings.py
+++ samachar/settings.py
@@ -8,6 +8,7 @@
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
+import os
 from urllib import quote_plus
 BOT_NAME = 'samachar'
 
